bce216f6b74955f691d04e267a7361b368a79185
Mikey Badr <mfbadr@gmail.com>
2014-09-16 15:40:39 -0500

    exercise form

--- client/views/exercises/exercises.js
+++ client/views/exercises/exercises.js
@@ -5,6 +5,20 @@
   .controller('ExercisesCtrl', ['$scope', 'Exercise', function($scope, Exercise){
     $scope.title = 'Add an exercise';
 
+    $scope.activities = [
+      {type: 'bicycling', level:[{name: 'bicycling-leisure', met: 6}, {name: 'bicycling-moderate', met: 8}, {name: 'bicycling-vigorous', met: 10}]},
+      {type: 'walking', level:[{name: 'walking-leisure', met: 3}, {name: 'walking-moderate', met: 4}, {name: 'walking-vigorous', met: 6}]},
+      {type: 'running', level:[{name: 'running-leisure', met: 7}, {name: 'running-moderate', met: 10}, {name: 'running-vigorous', met: 14}]},
+      {type: 'weights', level:[{name: 'weights-leisure', met: 3}, {name: 'weights-moderate', met: 5}, {name: 'weights-vigorous', met: 7}]},
+      {type: 'yoga', level:[{name: 'yoga-leisure', met: 2.5}, {name: 'yoga-moderate', met: 3.0}, {name: 'yoga-vigorous', met: 3.5}]},
+      {type: 'playing cards', level:[{name: 'playing cards-leisure', met: 1.2}, {name: 'playing cards-moderate', met: 1.5}, {name: 'playing cards-vigorous', met: 1.8}]},
+      {type: 'watching football', level:[{name: 'watching football-leisure', met: 1.0}, {name: 'watching football-moderate', met: 1.5}, {name: 'watching football-vigorous', met: 2.0}]},
+      {type: 'sex', level:[{name: 'sex-leisure', met: 1.5}, {name: 'sex-moderate', met: 2.0}, {name: 'sex-vigorous', met: 2.5}]}
+    ];
+
+    $scope.activity = {};
+    $scope.exercise = {};
+
     $scope.addExercise = function(){};
   }]);
 })();
