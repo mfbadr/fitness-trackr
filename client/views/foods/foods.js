8a2a42a14dc42e48fc48c4434cde076eea2a65c0
Logan Richardson <g.logan.richardson@gmail.com>
2014-09-17 00:32:39 -0500

    fixed foods.js

--- client/views/foods/foods.js
+++ client/views/foods/foods.js
@@ -2,13 +2,12 @@
   'use strict';
 
   angular.module('fitness-trackr')
-  .controller('FoodCtrl', ['$scope', 'Food', function($scope, Food){
+  .controller('FoodsCtrl', ['$scope', 'Food', function($scope, Food){
     $scope.food = {};
     $scope.foods = [];
 
     $scope.addFood = function(){
-      Food.create($scope.food).then(function(response){
-        console.log($scope.food);
+      Food.addFood($scope.food).then(function(response){
         $scope.foods.push(response.data.food);
         $scope.food = {};
         toastr.success('You successfully added food!');
